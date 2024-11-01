d699489b4711d2db83f61a5a922fb5d354e11d3b
Roberto Tores <rocatoro18@outlook.com>
2024-11-01 10:52:11 -0700

    .

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+  // SEÑAL COMPUTADA DE PURA LECTURA
+  public squareCounter = computed(() => this.counter() * this.counter());
+
+  increaseBy(value: number){
+
+    //this.counter.set(this.counter() + value);
+    this.counter.update(current => current + value);
+
+  }
 
 }
