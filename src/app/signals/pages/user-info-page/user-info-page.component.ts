97a49b8d15888ec8262907622d7c40a621edb3ed
Roberto Tores <rocatoro18@outlook.com>
2024-11-02 16:32:36 -0700

    .

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit{
+
+  private usersService = inject(UsersServiceService);
+  public userId = signal(1);
+
+  public currentUser = signal<User|undefined>(undefined);
+  private userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+  }
+
+  loadUser(id: number){
+    if(id <= 0) return;
+
+    // ESTO ES SINCRONO
+    this.userId.set(id);
+
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById(id)
+      .subscribe(user => {
+        //console.log({user});
+        // CON EL SET SE SOBRESCRIBE, NO SE MUTA O LLAMA EL VALOR ANTERIOR
+        this.currentUser.set(user)
+      })
+
+  }
 
 }
