e0d29552041be529405ec6729cb08a5452b0d620
Roberto Tores <rocatoro18@outlook.com>
2024-11-04 13:19:10 -0700

    .

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, OnDestroy, OnInit, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request';
 
 @Component({
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css'
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy, OnInit{
+
+  public counter = signal(10);
 
   public user = signal<User>({
     id: 1,
@@ -14,6 +16,31 @@ export class PropertiesPageComponent {
     last_name: 'Torres',
     avatar: 'https://reqres.in/img/faces/1-image.jpg'
   });
+
+  public fullName = computed(() => `${this.user().first_name} ${this.user().last_name}`);
+
+  public userChangedEffect = effect(() => {
+    //console.log('userChangedEffect triggered');
+    console.log( `${this.user().first_name} - ${this.counter()}`);
+  });
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.counter.update(current => current + 1);
+
+      if(this.counter() == 15) this.userChangedEffect.destroy();
+
+    },1000);
+  }
+
+  ngOnDestroy(): void {
+    // LIMPIAR EFECTOS MANUALMENTE
+    //this.userChangedEffect.destroy();
+  }
+
+  increaseBy(value: number){
+    this.counter.update(current => current + value);
+  }
 
   onFieldUpdated(field: keyof User, value: string) {
     //console.log({field,value});
@@ -53,7 +80,11 @@ export class PropertiesPageComponent {
           break;
       }
 
-      return current;
+      // NO FUNCIONA DE ESTA FORMA PORQUE ANGULAR NO DETECTA LAS MUTACIONES AL
+      // CAMBIAR DE ESTADO
+      //return current;
+      // SE TIENE QUE HACER DE ESTA FORMA PARA QUE ANGULAR DETECTE LAS MUTACIONES
+      return {...current};
     })
 
   }
